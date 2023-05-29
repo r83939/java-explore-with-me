0e0dc315b67d3cccd9fa37c027a91b249cd740be
EvgenyRom <r83939@mail.ru>
2023-05-29 17:08:51 +0300

    DP main_svc All tests pass

--- ewm-main-service/src/main/java/ru/practicum/user/repository/UserRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/user/repository/UserRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.user.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.practicum.user.model.User;
 
 import java.util.List;
